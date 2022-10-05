cb5ef1870e9e98af90abfb97630a5086914c9934
AnaBia25 <115107561+BeaSMonteiro@users.noreply.github.com>
2022-10-05 18:32:19 +0000

    Mudei o 7 para 77

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from circulo import *
 
-print("Branch desenvolvimento. Mudei o valor do argumento de 76 para 7")
+print("Branch desenvolvimento. Mudei o valor do argumento de 76 para 77")
 print('O raio do círculo é {:.2f}'.format(area(7)))
 print('O comprimento do círculo é {:.2f}'.format(comprimento_circunferencia(7)))
 
